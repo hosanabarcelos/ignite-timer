74feb57589a908f05c47d1d47162b70276a065ed
Hosana <hosanabarceloscandido@gmail.com>
2023-08-09 12:57:44 -0300

    feat: add rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './components/Router'
 
+export function App() {
   return (
-    <>
-      <div>
-        <p>Init</p>
-      </div>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
