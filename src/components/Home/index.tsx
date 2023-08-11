f1ded285b31efc03453b3189a85e3f912f250e8e
Hosana <hosanabarceloscandido@gmail.com>
2023-08-11 17:12:36 -0300

    feat: add implementação final de home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,6 +4,9 @@ import {
   FormContainer,
   CountdownContainer,
   Separator,
+  StartCountdownButton,
+  MinutesAmountInput,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Fazer módulo 79 do curso de Vue" />
+            <option value="Fazer café gourmet" />
+            <option value="Montar uma playlist de rock" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +48,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
