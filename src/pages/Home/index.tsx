2cb80efab3b58e0cb54e55a1610fe36e43f57ed1
Hosana <hosanabarceloscandido@gmail.com>
2023-09-18 17:46:26 -0300

    fix: corrige timer a partir de novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,12 +50,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSeconstsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -71,6 +77,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSeconstsPassed(0)
 
     reset()
   }
